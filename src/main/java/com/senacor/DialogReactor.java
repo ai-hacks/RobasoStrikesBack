72fd1ba0b14649dd475aa4d17c01d3c3da7ecb02
bseifert <bseifert@senacor.com>
2017-09-22 10:35:42 +0200

    merge fix

--- src/main/java/com/senacor/DialogReactor.java
+++ src/main/java/com/senacor/DialogReactor.java
@@ -8,17 +8,26 @@ class DialogReactor implements ReactToAnswer {
     private final SpeechRecognizer dialogRecognizer;
 
     private final Map<String, String> abbrevationMap;
+    private boolean isRobaso = false;
+
 
     public DialogReactor(SpeechRecognizer dialogRecognizer) {
         this.dialogRecognizer = dialogRecognizer;
         abbrevationMap = new AbbreviationImporter().getAbbrevationMap("baa.csv");
     }
 
+
     @Override
     public Reaction reactTo(SpeechResult answer) {
         String hypothesis = answer.getHypothesis();
         if (StateChangeDemo.UNKOWN_HYPOTHESIS.equals(hypothesis)) {
             return new Reaction(this, "Was");
+        }
+
+
+        if (!isRobaso && "robaso".equals(hypothesis)) {
+            isRobaso = true;
+            return new Reaction(this, "ok");
         }
 
         String utterance = answer.getHypothesis().trim();
@@ -29,6 +38,7 @@ class DialogReactor implements ReactToAnswer {
             return new Reaction(this, utterance + " bedeutet " + acro);
         }
 
+        isRobaso = false;
         System.out.println("#### undefined #### '" + utterance + '\'');
         return new Reaction(this, "Ich kann " + utterance + " nicht definieren");
     }
